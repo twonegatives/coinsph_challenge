0ff7c49c6262d02183e31d0803f5354530dde5b1
twonegatives <whitewhiteheaven@gmail.com>
2019-03-30 14:55:24 +0300

    replaced accounts creation from migrations

--- migrations/20190329025939-CreateAliceAccount.sql
+++
@@ -1,8 +0,0 @@
-
--- +migrate Up
-INSERT INTO accounts(name, balance, currency)
-VALUES ('alice456', 0, 'usd');
-
--- +migrate Down
-
-DELETE FROM accounts WHERE name = 'alice456';
--- migrations/20190329030320-CreateBobAccount.sql
+++
@@ -1,8 +0,0 @@
-
--- +migrate Up
-INSERT INTO accounts(name, balance, currency)
-VALUES ('bob123', 0, 'usd');
-
--- +migrate Down
-
-DELETE FROM accounts WHERE name = 'bob123';
